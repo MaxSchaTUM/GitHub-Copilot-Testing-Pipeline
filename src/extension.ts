2fd5c45510d51ff4ac8d71bb4c8358c43759111a
Max Schallermayer <schaller@in.tum.de>
2024-12-06 13:12:39 +0100

    Format time output

--- src/extension.ts
+++ src/extension.ts
@@ -31,7 +31,7 @@ const waitForStableCharacterCount = async (timeout = 60000) => {
 const BASE_PATH = "/Users/schaller/code/sqs_manual_experiment";
 const PROJECT_PATH = `${BASE_PATH}/jsoup`;
 // TODO make this not hard coded but argument to extension
-const CLASSES_PATH = `${BASE_PATH}/gentestcopilot/jsoup_classes_all.txt`; // contains list of relative path to classes within a project, one per line
+const CLASSES_PATH = `${BASE_PATH}/gentestcopilot/jsoup_classes_small.txt`; // contains list of relative path to classes within a project, one per line
 const REPORTS_FOLDER = `${BASE_PATH}/testReports/jsoup`;
 const LOG_FILE = `${BASE_PATH}/log.txt`;
 const JAVA_IMPORTER_PATH = `${BASE_PATH}/javaimports-1.5-all-deps.jar`;
@@ -179,9 +179,9 @@ export function activate(context: vscode.ExtensionContext) {
       logToFile("Done processing all classes");
       const endTime = new Date();
       logToFile(
-        `Total time taken in minutes: ${
+        `Total time taken in minutes: ${Math.floor(
           (endTime.getTime() - startTime.getTime()) / 60000
-        }`
+        )}`
       );
     }
   );
