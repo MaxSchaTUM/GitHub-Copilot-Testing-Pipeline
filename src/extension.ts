8dab9ec790aeba4e9daa505335077630b6764ed5
Max Schallermayer <schaller@in.tum.de>
2024-12-06 12:31:40 +0100

    Add time tracking

--- src/extension.ts
+++ src/extension.ts
@@ -30,7 +30,8 @@ const waitForStableCharacterCount = async (timeout = 60000) => {
 };
 const BASE_PATH = "/Users/schaller/code/sqs_manual_experiment";
 const PROJECT_PATH = `${BASE_PATH}/jsoup`;
-const CLASSES_PATH = `${BASE_PATH}/gentestcopilot/jsoup_classes_small.txt`; // contains list of relative path to classes within a project, one per line
+// TODO make this not hard coded but argument to extension
+const CLASSES_PATH = `${BASE_PATH}/gentestcopilot/jsoup_classes_all.txt`; // contains list of relative path to classes within a project, one per line
 const REPORTS_FOLDER = `${BASE_PATH}/testReports/jsoup`;
 const LOG_FILE = `${BASE_PATH}/log.txt`;
 const JAVA_IMPORTER_PATH = `${BASE_PATH}/javaimports-1.5-all-deps.jar`;
@@ -49,6 +50,8 @@ export function activate(context: vscode.ExtensionContext) {
     "gentestcopilot.helloWorld",
 
     async () => {
+      const startTime = new Date();
+
       const classes = await vscode.workspace.openTextDocument(CLASSES_PATH);
       const classesContent = classes.getText();
       const classesArray = classesContent.split("\n");
@@ -172,6 +175,12 @@ export function activate(context: vscode.ExtensionContext) {
         }
       }
       logToFile("Done processing all classes");
+      const endTime = new Date();
+      logToFile(
+        `Total time taken in minutes: ${
+          (endTime.getTime() - startTime.getTime()) / 60000
+        }`
+      );
     }
   );
 
